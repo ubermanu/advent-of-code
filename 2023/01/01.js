1f3e029e7e3332b02942819eb4166341d2d171ea
ubermanu <manu@paca.happy-dev.fr>
2023-12-02 21:13:30 +0100

    2023-01 part 2

--- 2023/01/01.js
+++ 2023/01/01.js
@@ -1,8 +1,43 @@
 import fs from 'fs/promises'
 
+const digits = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine'
+}
+
 /** @param {string} line */
-function get_first_digit(line) {
-  return line.match(/\d/)[0] ?? ''
+/** @param {boolean} rev */
+function get_first_digit(line, rev = false) {
+  let idx = -1
+  let digit = ''
+
+  if (rev) {
+    line = reverse(line)
+  }
+
+  const match = /\d/.exec(line)
+  if (match) {
+    idx = match.index
+    digit = match[0]
+  }
+
+  for (let d of Object.entries(digits)) {
+    const i = line.indexOf(rev ? reverse(d[1]) : d[1])
+    if (i > -1 && (i < idx || idx === -1)) {
+      idx = i
+      digit = d[0]
+    }
+  }
+
+  return digit
 }
 
 /** @param {string} str*/
@@ -14,7 +49,7 @@ function cal_sum(text) {
   const lines = text.trim().split("\n")
   let total = 0
   for (let line of lines) {
-    total += +get_first_digit(line).concat(get_first_digit(reverse(line)))
+    total += +get_first_digit(line).concat(get_first_digit(line, true))
   }
   console.log("Sum of calibrations values:", total)
 }
@@ -26,7 +61,18 @@ a1b2c3d4e5f
 treb7uchet
 `
 
+const doc2 = `
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`
+
 const file = await fs.readFile("input")
 
 cal_sum(doc)
+cal_sum(doc2)
 cal_sum(file.toString())

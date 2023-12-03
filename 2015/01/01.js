30ba9d1665b4a9a414754a01efd929dc44c74044
ubermanu <manu@paca.happy-dev.fr>
2023-12-03 21:28:55 +0100

    2015-01 part 2

--- 2015/01/01.js
+++ 2015/01/01.js
@@ -7,3 +7,24 @@ function get_floor(text) {
 }
 
 console.log("The floor is:", get_floor(file.toString()))
+
+function enters_basement_at(text) {
+  let level = 0
+
+  for (let i in text) {
+    const char = text[i]
+    if (char === '(') {
+      level++
+    }
+    if (char === ')') {
+      level--
+    }
+    if (level < 0) {
+      return i + 1
+    }
+  }
+
+  return 0
+}
+
+console.log("Enters basement at:", enters_basement_at(file.toString()))

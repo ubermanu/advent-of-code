3ca9adeeb570a115f743b59e5d2e037f9c426730
ubermanu <manu@paca.happy-dev.fr>
2023-12-03 23:29:38 +0100

    2015-02 part 2

--- 2015/02/02.zig
+++ 2015/02/02.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const print = std.debug.print;
 const parseInt = std.fmt.parseInt;
 
 fn get_box_surface(box: []const u8) !i32 {
@@ -13,11 +14,28 @@ fn get_box_surface(box: []const u8) !i32 {
     return (2 * l * w) + (2 * w * h) + (2 * h * l) + leftover;
 }
 
+fn get_ribbon_length(box: []const u8) !i32 {
+    var it = std.mem.split(u8, box, "x");
+
+    var w: i32 = try parseInt(i32, it.next().?, 10);
+    var l: i32 = try parseInt(i32, it.next().?, 10);
+    var h: i32 = try parseInt(i32, it.next().?, 10);
+
+    var sizes: [3]i32 = .{ w, l, h };
+
+    std.sort.heap(i32, sizes[0..], {}, std.sort.asc(i32));
+
+    var wrap_length = sizes[0] * 2 + sizes[1] * 2;
+    var bow_length = w * l * h;
+
+    return wrap_length + bow_length;
+}
+
 const file = @embedFile("input");
 
 pub fn main() !void {
-    std.debug.print("{d}\n", .{try get_box_surface("2x3x4")});
-    std.debug.print("{d}\n", .{try get_box_surface("1x1x10")});
+    print("{d}\n", .{try get_box_surface("2x3x4")});
+    print("{d}\n", .{try get_box_surface("1x1x10")});
 
     var it = std.mem.split(u8, file, "\n");
     var total: i32 = 0;
@@ -28,5 +46,21 @@ pub fn main() !void {
         }
     }
 
-    std.debug.print("Total surface need of paper: {d}\n", .{total});
+    print("Total surface of paper needed: {d}\n", .{total});
+
+    print("---\n", .{});
+
+    print("{d}\n", .{try get_ribbon_length("2x3x4")});
+    print("{d}\n", .{try get_ribbon_length("1x1x10")});
+
+    it.reset();
+    total = 0;
+
+    while (it.next()) |line| {
+        if (line.len > 0) {
+            total += try get_ribbon_length(line);
+        }
+    }
+
+    print("Total length of ribban needed: {d}\n", .{total});
 }

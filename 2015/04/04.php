69c633d65c04d40bd5014319a59e746a20c08003
ubermanu <manu@paca.happy-dev.fr>
2023-12-04 20:31:43 +0100

    2015-04 part 2

--- 2015/04/04.php
+++ 2015/04/04.php
@@ -1,11 +1,13 @@
 <?php
 
-function get_suffix(string $str): int
+function get_suffix(string $str, int $n = 5): int
 {
     $i = 0;
+    $zeros = str_pad("", $n, "0");
+    
     while (true) {
         $hash = md5($str . strval($i));
-        if (substr($hash, 0, 5) === '00000') {
+        if (substr($hash, 0, $n) === $zeros) {
             return $i;
         }
         $i++;
@@ -15,3 +17,4 @@ function get_suffix(string $str): int
 print get_suffix("abcdef") . PHP_EOL;
 print get_suffix("pqrstuv") . PHP_EOL;
 print get_suffix("bgvyzdsv") . PHP_EOL;
+print get_suffix("bgvyzdsv", 6) . PHP_EOL;

2538deaa61c8637575710fab46568e118b7fa05c
ubermanu <manu@paca.happy-dev.fr>
2023-12-04 21:37:48 +0100

    2015-05 part 2

--- 2015/05/05.php
+++ 2015/05/05.php
@@ -29,16 +29,12 @@ function is_nice(string $str): bool {
     return contains_three_vowels($str) && has_double_letter($str) && !has_forbidden_words($str);
 }
 
-function is_naughty(string $str): bool {
-    return !is_nice($str);
-}
-
 var_dump(is_nice('ugknbfddgicrmopn'));
 var_dump(is_nice('aaa'));
 
-var_dump(is_naughty('jchzalrnumimnmhp'));
-var_dump(is_naughty('haegwjzuvuyypxyu'));
-var_dump(is_naughty('dvszwmarrgswjxmb'));
+var_dump(!is_nice('jchzalrnumimnmhp'));
+var_dump(!is_nice('haegwjzuvuyypxyu'));
+var_dump(!is_nice('dvszwmarrgswjxmb'));
 
 $file = trim(file_get_contents("input"));
 
@@ -46,9 +42,82 @@ $total = 0;
 
 foreach (explode("\n", $file) as $line) {
     if (is_nice($line)) {
-        print $line . PHP_EOL;
         $total++;
     }
 }
 
 print "There are $total nice strings" . PHP_EOL;
+
+
+function has_letter_pair_twice(string $str): bool {
+    $pairs = [];
+    $chars = str_split($str);
+    for ($i = 0, $l = count($chars) - 1; $i < $l; $i++) {
+        $pairs[$i] = $chars[$i] . $chars[$i+1];
+    }
+
+    $count = [];
+    
+    foreach ($pairs as $index => $pair) {
+        if (isset($count[$pair])) {
+            $count[$pair][] = $index;
+        } else {
+            $count[$pair] = [$index];
+        }
+    }
+
+    foreach ($count as $pair => $indexes) {
+        if (count($indexes) === 1) {
+            continue;
+        }
+        foreach ($indexes as $i => $a) {
+            foreach ($indexes as $j => $b) {
+                if ($i === $j) {
+                    continue;
+                }
+                if ($a + 1 < $b || $b + 1 < $a) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+function has_letter_inbetween(string $str): bool {
+    if (strlen($str) < 3) {
+        return false;
+    }
+
+    $chars = str_split($str);
+
+    for ($i = 0, $l = count($chars) - 2; $i < $l; $i++) {
+        if ($chars[$i] === $chars[$i+2]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function is_nice2(string $str): bool {
+    return has_letter_pair_twice($str) && has_letter_inbetween($str);
+}
+
+var_dump(is_nice2("qjhvhtzxzqqjkmpb"));
+var_dump(is_nice2("xxyxx"));
+var_dump(!is_nice2("uurcxstgmygtbstg"));
+var_dump(!is_nice2("ieodomkazucvgmuy"));
+
+
+$total = 0;
+
+foreach (explode("\n", $file) as $line) {
+    if (is_nice2($line)) {
+        print $line . PHP_EOL;
+        $total++;
+    }
+}
+
+print "There are $total nicer strings" . PHP_EOL;

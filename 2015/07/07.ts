a4369325ce749565a0f48d47ef90ea40d96d21a2
ubermanu <manu@paca.happy-dev.fr>
2023-12-06 22:59:37 +0100

    2015-07 part 1

--- 2015/07/07.ts
+++ 2015/07/07.ts
@@ -22,7 +22,7 @@ function parse_instructions(text: string): Instruction[] {
   const lines = text.trim().split("\n")
 
   for (let line of lines) {
-    const [, cmd, to] = line.match(/(.*)\s->\s(\w)/) ?? []
+    const [, cmd, to] = line.match(/^(.*)\s->\s(\w+)$/) ?? []
 
     const inst: Instruction = {
       type: 'SET',
@@ -44,41 +44,123 @@ function parse_instructions(text: string): Instruction[] {
   return stack
 }
 
-type WireGrid = Record<string, number>
+class Gate {
+  active = false
+  type: Instruction['type']
+  inputs: Array<number | Wire> = []
+  output: Wire
+}
 
-function exec_instructions(instructions: Instruction[]): WireGrid {
-  const grid: WireGrid = {}
+class Wire {
+  value: number | undefined = undefined
+  inputs: Gate[] = []
 
-  for (let inst of instructions) {
-    switch (inst.type) {
-      case 'SET':
-        grid[inst.to] = /^\d+$/.test(inst.args[0]) ? +inst.args[0] : grid[inst.args[0]]
-        break
-      case 'AND':
-        grid[inst.to] = grid[inst.args[0]] & grid[inst.args[1]]
-        break
-      case 'OR':
-        grid[inst.to] = grid[inst.args[0]] | grid[inst.args[1]]
-        break
-      case 'NOT':
-        grid[inst.to] = ~grid[inst.args[0]] & 65535
-        break
-      case 'LSHIFT':
-        grid[inst.to] = grid[inst.args[0]] << +inst.args[1]
-        break
-      case 'RSHIFT':
-        grid[inst.to] = grid[inst.args[0]] >> +inst.args[1]
-        break
-    }
+  connect(input: Gate): void {
+    input.output = this
+    this.inputs.push(input)
+  }
+}
+
+class Circuit {
+  wires: Map<string, Wire> = new Map()
+}
+
+function is_number(str: string): boolean {
+  return /^\d+$/.test(str)
+}
+
+function connect_wires(instructions: Instruction[]): Circuit {
+  const c = new Circuit()
+
+  const wire_names = [... new Set(instructions.map(({ to }) => to))]
+
+  for (let name of wire_names) {
+    c.wires.set(name, new Wire())
   }
 
-  return grid
+  for (let inst of instructions) {
+    const g = new Gate()
+    g.type = inst.type
+
+    inst.args.forEach(arg => {
+      if (is_number(arg)) {
+        g.inputs.push(+arg)
+      } else {
+        g.inputs.push(c.wires.get(arg)!)
+      }
+    })
+
+    c.wires.get(inst.to)?.connect(g)
+  }
+
+  return c
+}
+
+const value = (v: Wire | number) => typeof v === 'number' ? v : v.value!
+
+function run_circuit(circuit: Circuit) {
+  let run = true
+
+  while (run) {
+    run = false
+
+    circuit.wires.forEach((wire) => {
+      wire.inputs.forEach((gate) => {
+
+        // Check if the gate can be activated
+        for (let input of gate.inputs) {
+          if (input instanceof Wire && input.value === undefined) {
+            // console.log("One of the gate inputs has no value yet")
+            run = true
+            return
+          }
+        }
+
+        const [arg1, arg2] = gate.inputs
+
+        switch (gate.type) {
+          case 'SET':
+            wire.value = value(arg1)
+            break
+          case 'AND':
+            wire.value = value(arg1) & value(arg2)
+            break
+          case 'OR':
+            wire.value = value(arg1) | value(arg2)
+            break
+          case 'NOT':
+            wire.value = ~value(arg1) & 65535
+            break
+          case 'LSHIFT':
+            wire.value = value(arg1) << value(arg2)
+            break
+          case 'RSHIFT':
+            wire.value = value(arg1) >> value(arg2)
+            break
+        }
+      })
+    })
+  }
+
+  console.log(circuit_values(circuit))
+}
+
+function circuit_values(circuit: Circuit): Record<string, number> {
+  return Object.fromEntries(
+    Array
+      .from(circuit.wires.entries())
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map(([name, wire]) => ([name, wire.value!]))
+  )
 }
 
 console.log('Document grid status:')
-console.log(exec_instructions(parse_instructions(doc)))
+let circuit = connect_wires(parse_instructions(doc));
+run_circuit(circuit)
 
 const file = await fs.readFile('input')
 
 console.log('Input grid status:')
-console.log(exec_instructions(parse_instructions(file.toString())))
+let circuit2 = connect_wires(parse_instructions(file.toString()));
+run_circuit(circuit2)
+
